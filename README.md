42212190177186918ea99c7a0de9b853715571c0
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-04-23 17:15:57 +0100

    Do logging correctly; Use bunyan for logging; Log requests' time; Add a error-handling middleware;

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ API for managing clubs, teams, competitions and seasons.
 ## TL;DR:
 
 1. [expressJS](https://expressjs.com/) to serve queries;
+1. [bunyan](https://github.com/trentm/node-bunyan) for logging;
+
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const bunyan = require('bunyan');
 const express = require('express');
 const package = require('./package.json');
 
 const app = express();
+const log = bunyan.createLogger({ name: package.name })
+
+app.use((req, res, next) => {
+  log.info(`${req.method} - ${req.path}`);
+  next();
+});
 
 app.get('/api/v1', (req, res) => res.json({ 'name': 'Hello World' }));
 
-app.listen(3000, () => console.log(`${package.name} listening on port 3000`));
+app.use(function(err, req, res, next) {
+  log.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(3000, () => log.info(`${package.name} listening on port 3000`));

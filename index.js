6d5cc2d9361b907090915f20892dc84178ed9758
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-04-23 17:40:39 +0100

    Move error-handlign middleware to a different module.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bunyan = require('bunyan');
 const express = require('express');
 const package = require('./package.json');
+const errorHandler = require('./middlewares/error');
 
 const app = express();
 const log = bunyan.createLogger({ name: package.name })
@@ -12,9 +13,6 @@ app.use((req, res, next) => {
 
 app.get('/api/v1', (req, res) => res.json({ 'name': 'Hello World' }));
 
-app.use(function(err, req, res, next) {
-  log.error(err);
-  res.status(500).send(err.message);
-});
+app.use(errorHandler(log));
 
 app.listen(3000, () => log.info(`${package.name} listening on port 3000`));

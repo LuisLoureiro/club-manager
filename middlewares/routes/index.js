7c39016524c76488b784ee3c95ade64991e8795b
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-04-23 18:49:04 +0100

    All requests should have a JSON payload; Parse it using the express built-in middleware;

--- middlewares/routes/index.js
+++ middlewares/routes/index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = logger => {
+  router.use(express.json());
   router.use((req, res, next) => {
-    logger.info(`${req.method} - ${req.path}`);
+    logger.info(`${req.method} ${req.path}`);
     next();
   });
 

25b9593bd94e6bfa5fd6d84288dfe0cbd53a81ef
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-04-24 14:56:10 +0100

    Create a modular route handler to handle all the requests to the 'Clubs' resource;

--- middlewares/routes/index.js
+++ middlewares/routes/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 
+const clubs = require('./clubs');
+
 const router = express.Router();
 
 module.exports = logger => {
   router.use(express.json());
+
   router.use((req, res, next) => {
-    logger.info(`${req.method} ${req.path}`);
+    logger.info(`${req.method} ${req.originalUrl}`);
     next();
   });
 
-  router.get('/', (req, res) => res.json({ 'name': 'Hello World' }));
+  router.use('/clubs', clubs);
 
   return router;
 }

ff9bb6e959c4a5bac3012b8f3895808df25bea57
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-04-27 19:25:38 +0100

    Handle request that returns the list of competitions.

--- api/competitions/routes.js
+++ api/competitions/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 
+const Competition = require('./model');
+
 const router = express.Router();
 
-router.get('/', (req, res) => res.sendStatus(501));
+router.get('/', (req, res, next) => {
+  Competition.find({}, (err, competitions) => {
+    if (err) {
+      next(err);
+    }
+
+    res.send(competitions);
+  });
+});
+
 router.get('/:id', (req, res) => res.sendStatus(501));
 
 router.post('/', (req, res) => res.sendStatus(501));

c5741c5043a107211e6ebb28a7b17ffc383cea7a
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-05-11 15:01:22 +0100

    Implement route handlers for the "Season" resource; Add tests.

--- api/seasons/routes.js
+++ api/seasons/routes.js
@@ -1,14 +1,54 @@
 const express = require('express');
 
+const Season = require('./model');
+const defaultHandler = require('../defaultEntityRequestHandler');
+
 const router = express.Router();
 
-router.get('/', (req, res) => res.sendStatus(501));
-router.get('/:id', (req, res) => res.sendStatus(501));
+router.use(express.json());
 
-router.post('/', (req, res) => res.sendStatus(501));
+router.get('/', (req, res, next) => {
+  Season.find({}, (err, seasons) => {
+    if (err) {
+      next(err);
+    }
 
-router.put('/:id', (req, res) => res.sendStatus(501));
+    res.send(seasons);
+  });
+});
 
-router.delete('/:id', (req, res) => res.sendStatus(501))
+router.get('/:id', (req, res, next) => {
+  Season.findById(req.params.id,
+    defaultHandler(res, next, season => res.send(season))
+  );
+});
+
+router.post('/', (req, res, next) => {
+  const name = req.body.name;
+
+  Season.create({ name }, (err, season) => {
+    if (err) {
+      next(err);
+    } else {
+      res.location(`${req.originalUrl}/${season.id}`).status(201).end();
+    }
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  if (!req.body.season) {
+    res.status(400).end();
+  } else {
+    Season.findByIdAndUpdate(req.params.id, req.body.season,
+      defaultHandler(res, next, season => res.sendStatus(204))
+    );
+  }
+});
+
+router.delete('/:id', (req, res, next) => {
+  Season.findByIdAndRemove(req.params.id,
+    defaultHandler(res, next, season => res.sendStatus(200))
+  );
+});
 
 module.exports = router;
---
+++ api/seasons/tests.js
@@ -0,0 +1,268 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const express = require('express');
+
+const Season = require('./model');
+const routes = require('./routes');
+const db = require('../../middlewares/database');
+
+const app = express();
+
+app.use('/seasons', routes);
+
+chai.use(chaiHttp);
+chai.should();
+
+
+describe('Test api/seasons', () => {
+
+  before(() => {
+    db.connect('test')
+      .then(() => console.log('test database is ready!'))
+      .catch(logErrorAndExit);
+  });
+
+  describe('GET /seasons', () => {
+
+    it('should return 200 and have a json type body with an empty array', done => {
+
+      chai.request(app)
+        .get('/seasons')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+
+          res.body.should.be.an('array').that.is.empty;
+
+          done();
+        });
+    });
+  });
+
+  describe('GET /seasons/:id', () => {
+
+    let insertedSeasons = [];
+
+    before(done => {
+      Season.insertMany([ { name: 'FirstSeason' } ], (err, seasons) => {
+        logErrorAndExit(err);
+
+        insertedSeasons = seasons;
+        done();
+      });
+    });
+
+    after(done => {
+      Season.deleteMany({}, err => {
+        logErrorAndExit(err);
+
+        done();
+      });
+    });
+
+    it('should return 404 when id doesn\'t exist', done => {
+
+      chai.request(app)
+        .get('/seasons/1')
+        .end((err, res) => {
+          res.should.have.status(404);
+
+          done();
+        });
+    });
+
+    it('should return a Season object when a valid id is given', done => {
+
+      chai.request(app)
+        .get(`/seasons/${insertedSeasons[0].id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+
+          res.body.should.be.an('object').that.includes({ name: 'FirstSeason' });
+
+          done();
+        });
+    });
+  });
+
+  describe('POST /seasons', () => {
+
+    after(done => {
+      Season.deleteMany({}, err => {
+        logErrorAndExit(err);
+
+        done();
+      });
+    });
+
+    it('should create a new season', done => {
+
+      const season = { name: 'PostSeason' };
+
+      chai.request(app)
+        .post('/seasons')
+        .send(season)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.should.have.header('Location', /^\/seasons\/.*/);
+
+          done();
+        });
+    });
+
+    it('should have one season created', done => {
+
+      Season.find({}, (err, res) => {
+        res.should.have.lengthOf(1);
+
+        done();
+      });
+    });
+  });
+
+  describe('PUT /seasons/:id', () => {
+
+    let insertedSeasons = [];
+
+    beforeEach(done => {
+      Season.insertMany([ { name: 'TestPUTSeason' } ], (err, seasons) => {
+        logErrorAndExit(err);
+
+        insertedSeasons = seasons;
+        done();
+      });
+    });
+
+    afterEach(done => {
+      Season.deleteMany({}, err => {
+        logErrorAndExit(err);
+
+        done();
+      });
+    });
+
+    it('should return 400 when no season object is passed in the body', done => {
+
+      chai.request(app)
+        .put(`/seasons/${insertedSeasons[0].id}`)
+        .end((err, res) => {
+          res.should.have.status(400);
+
+          done();
+        });
+    });
+
+    it('should return 404 when id doesn\'t exist', done => {
+
+      const changedSeason = { season: {} };
+
+      chai.request(app)
+        .put('/seasons/1')
+        .send(changedSeason)
+        .end((err, res) => {
+          res.should.have.status(404);
+
+          done();
+        });
+    });
+
+    it('should update season when a valid id is given', done => {
+
+      const changedSeason = { season: { name: 'UpdatedSeason' } };
+
+      chai.request(app)
+        .put(`/seasons/${insertedSeasons[0].id}`)
+        .send(changedSeason)
+        .end((err, res) => {
+
+          res.should.have.status(204);
+
+          Season.find({}, (err, seasons) => {
+
+            seasons.should.be.an('array').that.has.lengthOf(1);
+            seasons[0].name.should.be.equal('UpdatedSeason');
+
+            done();
+          });
+        });
+    });
+
+    it('should return 204 but shouldn\'t update season when no property is passed', done => {
+
+      const changedSeason = { season: {} };
+
+      chai.request(app)
+        .put(`/seasons/${insertedSeasons[0].id}`)
+        .send(changedSeason)
+        .end((err, res) => {
+
+          res.should.have.status(204);
+
+          Season.find({}, (err, seasons) => {
+
+            seasons.should.be.an('array').that.has.lengthOf(1);
+            seasons[0].name.should.be.equal('TestPUTSeason');
+
+            done();
+          });
+        });
+    });
+  });
+
+  describe('DELETE /seasons/:id', () => {
+
+    let insertedSeasons = [];
+
+    before(done => {
+      Season.insertMany([ { name: 'TestDELETESeason' } ], (err, seasons) => {
+        logErrorAndExit(err);
+
+        insertedSeasons = seasons;
+        done();
+      });
+    });
+
+    after(done => {
+      Season.deleteMany({}, err => {
+        logErrorAndExit(err);
+
+        done();
+      });
+    });
+
+    it('should return 404 when id doesn\'t exist', done => {
+
+      chai.request(app)
+        .delete('/seasons/1')
+        .end((err, res) => {
+          res.should.have.status(404);
+
+          done();
+        });
+    });
+
+    it('should remove season and return 200', done => {
+
+      chai.request(app)
+        .delete(`/seasons/${insertedSeasons[0].id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+
+          Season.find({}, (err, seasons) => {
+
+            seasons.should.be.an('array').that.is.empty;
+
+            done();
+          });
+        });
+    });
+  });
+});
+
+function logErrorAndExit(err) {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+}
